2a2aee3334bd63607be1e2635013474c0516eb9c
Travis Erard <travis@impossiblecreature.com>
2015-05-17 10:57:54 -0500

    add ostruct to handle prototyped data structure

--- app/controllers/auctions_controller.rb
+++ app/controllers/auctions_controller.rb
@@ -4,8 +4,11 @@ class AuctionsController < ApplicationController
   def index
     #Struct.new("Auction", :name, :address)
     #@auction = Struct.new(Auction, :name).new
+    @auctions = []
 
-    @auction = OpenStruct.new
-    @auction.name = "maggie"
+    names = ["maggie", "finley", "sookie", "higgins"]
+    names.each do |name|
+      @auctions << OpenStruct.new(name: name)
+    end
   end
 end
